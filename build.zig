18f4b71ae02f0965b8a3120422289f7eea0a0bce
Ralph Caraveo <deckarep@gmail.com>
2024-05-05 19:34:03 -0700

    Tweaking build scripts and module name

--- build.zig
+++ build.zig
@@ -1,10 +1,25 @@
 const std = @import("std");
-const Build = std.build;
 
-pub fn build(b: *Build) void {
-    const ziglangSetMod = b.addModule("ziglangset", .{
+pub fn build(b: *std.Build) void {
+    // Standard optimize option allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    const ziglangSetMod = b.addModule("ziglang-set", .{
         .root_source_file = .{.path = "src/set.zig"},
     });
 
     _ = ziglangSetMod;
+
+    const main_tests = b.addTest(.{
+        .name = "ziglang-set tests",
+        .root_source_file = .{ .path = "src/set.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_main_tests = b.addRunArtifact(main_tests);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&run_main_tests.step);
 }
